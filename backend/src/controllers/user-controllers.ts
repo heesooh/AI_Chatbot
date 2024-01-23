a5789907db138c9116f0c5fc1f90c1a6470aa742
heesooh <heesoohwang1231@gmail.com>
2024-01-22 18:17:34 -0800

    Implemented user login, password and email validation functions.

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import User from "../models/User.js"
-import { hash } from "bcrypt";
+import { hash, compare } from "bcrypt";
 
 export const getAllUsers = async (
     req:Request,
@@ -29,6 +29,10 @@ export const userSignup = async (
     ) => {
         try {
             const {name, email, password} = req.body;
+            const existingUser = await User.findOne({email});
+            if (existingUser) {
+                return res.status(401).send("User already registered!");
+            }
             const hashedPassword = await hash(password, 10);
             const user = new User({name, email, password:hashedPassword});
             await user.save() // save user to the MongoDB
@@ -37,6 +41,36 @@ export const userSignup = async (
             return res.status(201).json({
                 message: "OK",
                 id: user._id.toString(),
+            });
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({
+                message: "ERROR",
+                cause: error.message,
+            });
+        }
+}
+
+export const userLogin = async (
+    req:Request,
+    res:Response,
+    next:NextFunction
+    ) => {
+        try {
+            const {email, password} = req.body;
+            const existingUser = await User.findOne({email});
+            if (!existingUser) {
+                return res.status(401).send("User does not exist!");
+            }
+
+            const isCorrectPassword = await compare(password, existingUser.password);
+            if (!isCorrectPassword) {
+                return res.status(403).send("Incorrect password!");
+            }
+
+            return res.status(201).json({
+                message: "OK",
+                id: existingUser._id.toString(),
             });
         } catch (error) {
             console.log(error);
