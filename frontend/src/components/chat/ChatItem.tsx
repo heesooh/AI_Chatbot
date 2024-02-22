fd3f1928add90e1547ce72bce1489096172524ed
heesooh <heesoohwang1231@gmail.com>
2024-02-21 17:52:06 -0800

    Updated the chat page UI.

--- frontend/src/components/chat/ChatItem.tsx
+++ frontend/src/components/chat/ChatItem.tsx
@@ -30,7 +30,17 @@ const ChatItem = ({ content, role }: { content: string; role: string }) => {
   const auth = useAuth();
   const messageBlocks = extractCodeFromString(content);
   return role.toLowerCase() == "assistant" ? (
-    <Box sx={{ display: "flex", p: 2, bgcolor: "#004d5612", my: 2, gap: 2 }}>
+    <Box
+      sx={{
+        display: "flex",
+        p: 2,
+        bgcolor: "#004d5612",
+        my: 2,
+        gap: 2,
+        borderRadius: "10px",
+        background: "linear-gradient(to bottom right, rgb(50, 36, 64), rgb(18, 33, 66), rgb(5, 12, 22))"
+      }}
+    >
       <Avatar sx={{ ml: "0" }}>
         <img src="openai.png" alt="openai" width={"30px"} />
       </Avatar>
@@ -42,22 +52,51 @@ const ChatItem = ({ content, role }: { content: string; role: string }) => {
           messageBlocks.length &&
           messageBlocks.map((block, index) =>
             isCodeBlock(block) ? (
-              <SyntaxHighlighter style={coldarkDark} language="javascript" key={index}>
+              <SyntaxHighlighter
+                style={coldarkDark}
+                language="javascript"
+                key={index}
+              >
                 {block}
               </SyntaxHighlighter>
             ) : (
-              <Typography sx={{ fontSize: "20px" }} key={index}>{block}</Typography>
+              <Typography sx={{ fontSize: "20px" }} key={index}>
+                {block}
+              </Typography>
             )
           )}
       </Box>
     </Box>
   ) : (
-    <Box sx={{ display: "flex", p: 2, bgcolor: "#004d56", my: 2, gap: 2 }}>
+    <Box
+      sx={{
+        display: "flex",
+        p: 2,
+        bgcolor: "#004d56",
+        my: 2,
+        gap: 2,
+        borderRadius: "10px",
+      }}
+    >
       <Avatar sx={{ ml: "0", bgcolor: "black", color: "white" }}>
-        {auth?.user?.name[0]}
-        {auth?.user?.name.split(" ")[1][0]}
+        {auth?.user?.name.includes(" ") ? (
+          <div>
+            {auth?.user?.name[0]}
+            {auth?.user?.name.split(" ")[1][0]}
+          </div>
+        ) : (
+          <div>
+            {auth?.user?.name[0]}
+          </div>
+        )}
       </Avatar>
-      <Box>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
         <Typography fontSize={"20px"}>{content}</Typography>
       </Box>
     </Box>
