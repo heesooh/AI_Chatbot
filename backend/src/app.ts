b00e75beb2fcbc846a437eab6b33e1187f375014
heesooh <heesoohwang1231@gmail.com>
2024-02-22 12:44:07 -0800

    Updated the backend COR policy to allow frontend to gain access.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,7 +11,7 @@ const app = express();
 // middlewares:
 // whitelist frontend port 5173
 // app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-app.use(cors({ origin: "https://heesoo-hwang-backend.onrender.com", credentials: true }));
+app.use(cors({ origin: "https://heesoo-hwang.onrender.com", credentials: true }));
 
 // JSON parser
 app.use(express.json());
